5d2d5207542f4f54112c93b8d551e117a91921d6
Karina Martinez Guerrero <karinamguerrero@estudiantec.cr>
2020-10-18 21:54:43 -0600

    Inicio de página de ventas

--- bike-store/src/app/app.module.ts
+++ bike-store/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SalePointComponent } from './Sales/sale-point/sale-point.component';
+import { SalesReportComponent } from './Administration/sales-report/sales-report.component';
+import { OrdersReportComponent } from './Administration/orders-report/orders-report.component';
+import { PurchasesReportComponent } from './Administration/purchases-report/purchases-report.component';
+import { ProductCategoryReportComponent } from './Administration/product-category-report/product-category-report.component';
+import { ReportsComponent } from './Administration/reports/reports.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavigationComponent } from './Miscellaneoues/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalePointComponent,
+    SalesReportComponent,
+    OrdersReportComponent,
+    PurchasesReportComponent,
+    ProductCategoryReportComponent,
+    ReportsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

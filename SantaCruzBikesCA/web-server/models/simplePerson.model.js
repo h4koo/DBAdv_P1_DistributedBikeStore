dac5311bd79b20f5f78b188bbe7c352259a0ee35
h4koo <georgeaz56@gmail.com>
2020-10-25 21:31:43 -0600

    employees by store

--- SantaCruzBikesCA/web-server/models/simplePerson.model.js
+++ SantaCruzBikesCA/web-server/models/simplePerson.model.js
@@ -9,7 +9,7 @@ const SimplePerson = function (name, lastname) {
 
 
 SimplePerson.getClients = result => {
-    sql.query("SELECT idCliente AS person_id, CONCAT(nombre,' ',apellido) AS name FROM ventas.clientes", (err, res) => {
+    sql.query("SELECT idCliente AS person_id, CONCAT(nombre,' ',apellido) AS name FROM ventas.clientes;", (err, res) => {
         if (err) {
             console.log("error: ", err);
             result(err, null);
@@ -21,7 +21,7 @@ SimplePerson.getClients = result => {
 };
 
 SimplePerson.getEmployees = result => {
-    sql.query("SELECT idEmpleado as person_id, CONCAT(nombre, ' ', apellido) as name FROM ventas.empleados", (err, res) => {
+    sql.query("SELECT idEmpleado as person_id, CONCAT(nombre, ' ', apellido) as name FROM ventas.empleados WHERE idTienda = 1;", (err, res) => {
         if (err) {
             console.log("error: ", err);
             result(err, null);

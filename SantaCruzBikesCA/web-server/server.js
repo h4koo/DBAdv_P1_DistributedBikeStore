5b73ce8eb8db1d204bf10a09ddd9a9a110e9f7e6
h4koo <georgeaz56@gmail.com>
2020-10-25 19:34:35 -0600

    error handling

--- SantaCruzBikesCA/web-server/server.js
+++ SantaCruzBikesCA/web-server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 
@@ -9,7 +9,7 @@ var corsOptions = {
   origin: "http://localhost:4200"
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
@@ -17,17 +17,17 @@ app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(process.cwd()+'/bike-store/dist/bike-store'))
+app.use(express.static(process.cwd() + '/bike-store/dist/bike-store'))
 
 // simple route
-app.get("/", (req, res) => {
-    //res.json({ message: "Welcome to DistributedBikes application." });
-    res.sendFile(process.cwd()+'/bike-store/dist/bike-store/index.html');
-});
+// app.get("/", (req, res) => {
+//     //res.json({ message: "Welcome to DistributedBikes application." });
+//     res.sendFile(process.cwd()+'/bike-store/dist/bike-store/index.html');
+// });
 
 require("./routes/routes.js")(app);
 
 // set port, listen for requests
 app.listen(3000, () => {
-    console.log("Server is running on port 3000.");
+  console.log("Server is running on port 3000.");
 });
